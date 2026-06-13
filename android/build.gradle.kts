b3073387c57c5f379923aed8c7ed7605b09a7de0
fahrulriadu22 <kojengkangoyi@gmail.com>
2026-06-13 12:10:35 +0700

    fix: remove version from flutter-plugin-loader and add google-services to plugins

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -18,7 +18,7 @@ pluginManagement {
 }
 
 plugins {
-    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+    id("dev.flutter.flutter-plugin-loader")
     id("com.android.application") version "9.0.1" apply false
     id("org.jetbrains.kotlin.android") version "2.3.20" apply false
     id("com.google.gms.google-services") version "4.4.0" apply false
